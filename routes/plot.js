0ba7656bcc00d404e7f114418ba0ed275c0e0eb5
Liam Fruzyna <liamfruzyna@gmail.com>
2021-03-25 20:04:43 -0500

    Plot by category (fixed #26)

--- routes/plot.js
+++ routes/plot.js
@@ -16,6 +16,7 @@ router.get('/', function(req, res, next)
   utils.session_exists(connection, req, res, function (user_id)
   {
     let account   = utils.create_clause(req.query.account, 'AND a.name = "$VALUE"')
+    let category  = utils.create_clause(req.query.category, 'AND t.category = "$VALUE"')
     let before    = utils.create_clause(req.query.before, `AND datediff(t.date, ${utils.process_date(req.query.before)}) < 0`)
     let after     = utils.create_clause(req.query.after, `AND datediff(t.date, ${utils.process_date(req.query.after)}) > 0`)
 
@@ -26,31 +27,41 @@ router.get('/', function(req, res, next)
       {
         accounts = {}
       }
-      connection.query(`(SELECT date_format(t.date, "%Y-%m-%d") as date, sum(t.amount) as raw, format(sum(t.amount), 2) as amount
-                          FROM transactions as t
-                          INNER JOIN accounts as a ON a.id = t.account_id
-                          WHERE t.user_id = ${user_id} ${account} ${before} ${after}
-                          GROUP BY date)
-                        UNION
-                        (SELECT max(date) as date, sum(t.amount) as raw, format(sum(t.amount), 2) as amount
-                          FROM transactions as t
-                          INNER JOIN accounts as a ON a.id = t.account_id
-                          WHERE t.user_id = ${user_id} ${account} ${before})
-                        ORDER BY date`, function (error, results, fields)
+      connection.query(`SELECT distinct category as name
+                        FROM transactions
+                        WHERE user_id = ${user_id}
+                        GROUP BY category`, function (error, categories, fields)
       {
-        if (error)
+        if (error || categories.length <= 0)
         {
-          console.log(error)
-          res.render('plot', { error_text: 'Error getting plot data', title: 'Finance | Plot', totals: [], query: req.query, accounts: accounts })
+          categories = {}
         }
-        else if (results.length > 0)
+        connection.query(`(SELECT date_format(t.date, "%Y-%m-%d") as date, sum(t.amount) as raw, format(sum(t.amount), 2) as amount
+                            FROM transactions as t
+                            INNER JOIN accounts as a ON a.id = t.account_id
+                            WHERE t.user_id = ${user_id} ${account} ${before} ${after} ${category}
+                            GROUP BY date)
+                          UNION
+                          (SELECT max(date) as date, sum(t.amount) as raw, format(sum(t.amount), 2) as amount
+                            FROM transactions as t
+                            INNER JOIN accounts as a ON a.id = t.account_id
+                            WHERE t.user_id = ${user_id} ${account} ${before} ${category})
+                          ORDER BY date`, function (error, results, fields)
         {
-          res.render('plot', { error_text: '', title: 'Finance | Plot', totals: results, query: req.query, accounts: accounts })
-        }
-        else
-        {
-          res.redirect('/add')
-        }
+          if (error)
+          {
+            console.log(error)
+            res.render('plot', { error_text: 'Error getting plot data', title: 'Finance | Plot', totals: [], query: req.query, accounts: accounts, categories: categories })
+          }
+          else if (results.length > 0)
+          {
+            res.render('plot', { error_text: '', title: 'Finance | Plot', totals: results, query: req.query, accounts: accounts, categories: categories })
+          }
+          else
+          {
+            res.redirect('/add')
+          }
+        })
       })
     })
   })
